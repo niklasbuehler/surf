79b775cb680e9a57dda404f49b57f67bc7a954fa
niklasbuehler <niklasbuehler27@gmail.com>
2019-03-30 10:55:40 +0100

    Clean up window titles

--- surf.c
+++ surf.c
@@ -635,11 +635,10 @@ updatetitle(Client *c)
 		getpagestats(c);
 
 		if (c->progress != 100)
-			title = g_strdup_printf("[%i%%] %s:%s | %s",
-			        c->progress, togglestats, pagestats, name);
+			title = g_strdup_printf("[%i%%] %s",
+			        c->progress, name);
 		else
-			title = g_strdup_printf("%s:%s | %s",
-			        togglestats, pagestats, name);
+			title = g_strdup_printf("%s", name);
 
 		gtk_window_set_title(GTK_WINDOW(c->win), title);
 		g_free(title);
